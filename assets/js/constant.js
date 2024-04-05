a2bfca1ca2320fc69b8a260744483ad708d9e285
leho-dev <phanleho002@gmail.com>
2024-04-05 08:55:16 +0700

    chore: format code with .prettierrc (#3)

--- assets/js/constant.js
+++ assets/js/constant.js
@@ -1,10 +1,10 @@
 const _IGNORE_SUBJECT_INIT = [
-  '_', // BHYT, Đồng phục thể dục
-  'MEETING', // Sinh hoạt lớp
-  'PEDU', // Giáo dục thể chất
-  'DEDU', // Giáo dục quốc phòng
-  'GENG0', // Tiếng Anh căn bản
-  'TEST', // Kiểm tra đầu vào
+    '_', // BHYT, Đồng phục thể dục
+    'MEETING', // Sinh hoạt lớp
+    'PEDU', // Giáo dục thể chất
+    'DEDU', // Giáo dục quốc phòng
+    'GENG0', // Tiếng Anh căn bản
+    'TEST' // Kiểm tra đầu vào
 ];
 
 const _URL_TIENICHSV = 'https://tienichsv.ou.edu.vn/#/diem';
--- assets/js/tienichsv.js
+++ assets/js/tienichsv.js
@@ -5,101 +5,93 @@ const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 const getInitData = () => {
-  const URL = window.location.href;
-  let userId = 'NOT FOUND';
-  let fullName = 'NOT FOUND';
+    const URL = window.location.href;
+    let userId = 'NOT FOUND';
+    let fullName = 'NOT FOUND';
 
-  const loginElement = document.querySelectorAll('app-login table > tr');
+    const loginElement = document.querySelectorAll('app-login table > tr');
 
-  if (loginElement.length > 0) {
-    userId = loginElement[0].querySelectorAll('td')[1].innerText.trim();
-    fullName = loginElement[1].querySelectorAll('td')[1].innerText.trim();
-  }
+    if (loginElement.length > 0) {
+        userId = loginElement[0].querySelectorAll('td')[1].innerText.trim();
+        fullName = loginElement[1].querySelectorAll('td')[1].innerText.trim();
+    }
 
-  (async () => {
-    await chrome.runtime.sendMessage({
-      type: 'INIT_URL',
-      payload: { URL, userId, fullName },
-    });
-  })();
+    (async () => {
+        await chrome.runtime.sendMessage({
+            type: 'INIT_URL',
+            payload: { URL, userId, fullName }
+        });
+    })();
 };
 
 const getData = () => {
-  const tableElement = document.querySelectorAll(
-    'table#excel-table > tbody > tr.text-center.ng-star-inserted'
-  );
+    const tableElement = document.querySelectorAll('table#excel-table > tbody > tr.text-center.ng-star-inserted');
 
-  const data = Array.from(tableElement).map((d) => {
-    const columns = d.querySelectorAll('td');
-    return {
-      code: columns[1].innerText,
-      name: columns[3].innerText,
-      credit: columns[4].innerText,
-      point: columns[10].innerText,
-    };
-  });
+    const data = Array.from(tableElement).map((d) => {
+        const columns = d.querySelectorAll('td');
+        return {
+            code: columns[1].innerText,
+            name: columns[3].innerText,
+            credit: columns[4].innerText,
+            point: columns[10].innerText
+        };
+    });
 
-  (async () => {
-    await chrome.runtime.sendMessage({ type: 'GET_DATA', payload: { data } });
-  })();
+    (async () => {
+        await chrome.runtime.sendMessage({ type: 'GET_DATA', payload: { data } });
+    })();
 };
 
 (() => {
-  return {
-    ignoreList: _IGNORE_SUBJECT_INIT,
-    data: [],
-    init() {
-      injectScriptActiveTab(getInitData);
-    },
-    subscribe() {
-      const notCorrectURL = () => {
-        $('.root').classList.add('hide');
-        $('.btn-import-data').classList.add('hide');
-      };
+    return {
+        ignoreList: _IGNORE_SUBJECT_INIT,
+        data: [],
+        init() {
+            injectScriptActiveTab(getInitData);
+        },
+        subscribe() {
+            chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+                const { type, payload } = request;
 
-      chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        const { type, payload } = request;
+                switch (type) {
+                    case 'INIT_URL':
+                        const { URL, userId, fullName } = payload;
+                        if (!URL) {
+                            $('.site-current').innerText = 'Lỗi không tìm thấy tab!';
+                            return;
+                        }
 
-        switch (type) {
-          case 'INIT_URL':
-            const { URL, userId, fullName } = payload;
-            if (!URL) {
-              notCorrectURL();
-              $('.site-current').innerText = 'Lỗi không tìm thấy tab!';
-              return;
-            }
+                        if (!URL.includes(_URL_TIENICHSV)) {
+                            $('.site-current').innerHTML = 'Vui lòng chuyển sang tienichsv.ou.edu.vn/#/diem';
+                            return;
+                        }
 
-            if (!URL.includes(_URL_TIENICHSV)) {
-              notCorrectURL();
-              $('.site-current').innerHTML =
-                'Vui lòng chuyển sang tienichsv.ou.edu.vn/#/diem';
-              return;
-            }
+                        $('.root').classList.remove('hide');
+                        $('.btn-import-data').classList.remove('hide');
+                        $('.site-current').innerText = userId + ' - ' + fullName;
+                        return;
 
-            $('.site-current').innerText = userId + ' - ' + fullName;
-            return;
+                    case 'GET_DATA':
+                        const { data } = payload;
 
-          case 'GET_DATA':
-            const { data } = payload;
+                        const dataFiltered = data.filter((d) => {
+                            const isIgnore = this.ignoreList.find((i) => d.code.includes(i));
+                            if (!isIgnore) return true;
+                        });
 
-            const dataFiltered = data.filter((d) => {
-              const isIgnore = this.ignoreList.find((i) => d.code.includes(i));
-              if (!isIgnore) return true;
+                        this.data = dataFiltered;
+                        this.render();
+                        return;
+                    default:
+                        console.log('No type match!');
+                        return;
+                }
             });
-
-            this.data = dataFiltered;
-            this.render();
-            return;
-          default:
-            console.log('No type match!');
-            return;
-        }
-      });
-    },
-    render() {
-      const tableHtmls = this.data
-        .map((d, idx) => {
-          return `
+        },
+        render() {
+            const tableHtmls = this.data
+                .map((d, idx) => {
+                    return `
           <tr data-id=${idx} class="row-data">
             <td>${d.code}</td>
             <td>${d.name}</td>
@@ -108,92 +100,85 @@ const getData = () => {
             <td data-id=${idx} class="delete" style="min-width: 20px;">x</td>
           </tr>
         `;
-        })
-        .join('');
+                })
+                .join('');
 
-      const ignoreListHtmls = this.ignoreList
-        .map((ig) => `<b>${ig}</b>`)
-        .join('; ');
+            const ignoreListHtmls = this.ignoreList.map((ig) => `<b>${ig}</b>`).join('; ');
 
-      const totalCredit = this.data.reduce(
-        (acc, curr) => acc + parseInt(curr.credit),
-        0
-      );
+            const totalCredit = this.data.reduce((acc, curr) => acc + parseInt(curr.credit), 0);
 
-      const avg = this.data.reduce(
-        (acc, curr) => {
-          const point = parseFloat(curr.point);
-          const credit = parseInt(curr.credit);
+            const avg = this.data.reduce(
+                (acc, curr) => {
+                    const point = parseFloat(curr.point);
+                    const credit = parseInt(curr.credit);
 
-          if (isNaN(point) || isNaN(credit)) return acc;
-          return {
-            point: acc.point + point * credit,
-            credit: acc.credit + credit,
-          };
+                    if (isNaN(point) || isNaN(credit)) return acc;
+                    return {
+                        point: acc.point + point * credit,
+                        credit: acc.credit + credit
+                    };
+                },
+                {
+                    point: 0,
+                    credit: 0
+                }
+            );
+
+            $('table.data').innerHTML = tableHtmls;
+            $('.ignore-list > span').innerHTML = ignoreListHtmls;
+            $('.total-credit span').innerText = totalCredit;
+            $('.avg-point span').innerText = (avg.point / avg.credit).toFixed(4);
         },
-        {
-          point: 0,
-          credit: 0,
+        handle() {
+            $('.btn-import-data').addEventListener('click', () => injectScriptActiveTab(getData));
+
+            $('table.data').onclick = (e) => {
+                const rowData = e.target.closest('.row-data');
+                const deleteBtn = e.target.closest('.delete');
+
+                if (rowData) {
+                    const id = rowData.dataset.id;
+                    const { code, name, credit, point } = this.data[id];
+
+                    $('input.code').value = code;
+                    $('input.name').value = name;
+                    $('input.credit').value = credit;
+                    $('input.point').value = point;
+                }
+
+                if (deleteBtn) {
+                    const id = deleteBtn.dataset.id;
+                    this.data.splice(id, 1);
+                    this.render();
+                }
+            };
+
+            $('.form-subject').onsubmit = (e) => {
+                e.preventDefault();
+
+                const code = $('input.code').value.trim();
+                const name = $('input.name').value.trim();
+                const credit = $('input.credit').value.trim();
+                const point = $('input.point').value.trim();
+
+                const data = { code, name, credit, point };
+
+                const isExist = this.data.find((d) => d.code == code);
+
+                if (!isExist) this.data.unshift(data);
+                else {
+                    const idx = this.data.findIndex((d) => d.code === code);
+                    this.data[idx] = data;
+                }
+
+                this.render();
+            };
+        },
+        start() {
+            this.subscribe();
+            this.init();
+            this.handle();
+            this.render();
         }
-      );
-
-      $('table.data').innerHTML = tableHtmls;
-      $('.ignore-list > span').innerHTML = ignoreListHtmls;
-      $('.total-credit span').innerText = totalCredit;
-      $('.avg-point span').innerText = (avg.point / avg.credit).toFixed(4);
-    },
-    handle() {
-      $('.btn-import-data').addEventListener('click', () =>
-        injectScriptActiveTab(getData)
-      );
-
-      $('table.data').onclick = (e) => {
-        const rowData = e.target.closest('.row-data');
-        const deleteBtn = e.target.closest('.delete');
-
-        if (rowData) {
-          const id = rowData.dataset.id;
-          const { code, name, credit, point } = this.data[id];
-
-          $('input.code').value = code;
-          $('input.name').value = name;
-          $('input.credit').value = credit;
-          $('input.point').value = point;
-        }
-
-        if (deleteBtn) {
-          const id = deleteBtn.dataset.id;
-          this.data.splice(id, 1);
-          this.render();
-        }
-      };
-
-      $('.form-subject').onsubmit = (e) => {
-        e.preventDefault();
-
-        const code = $('input.code').value.trim();
-        const name = $('input.name').value.trim();
-        const credit = $('input.credit').value.trim();
-        const point = $('input.point').value.trim();
-
-        const data = { code, name, credit, point };
-
-        const isExist = this.data.find((d) => d.code == code);
-
-        if (!isExist) this.data.unshift(data);
-        else {
-          const idx = this.data.findIndex((d) => d.code === code);
-          this.data[idx] = data;
-        }
-
-        this.render();
-      };
-    },
-    start() {
-      this.subscribe();
-      this.init();
-      this.handle();
-      this.render();
-    },
-  };
+    };
 })().start();
