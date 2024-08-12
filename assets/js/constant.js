9eb6a6f3376b0a42dc424f60ff762c0367c72956
leho-dev <phanleho002@gmail.com>
2024-08-12 13:42:30 +0700

    test: test automatic release

--- assets/js/constant.js
+++ assets/js/constant.js
@@ -1,21 +1,26 @@
 export const _IGNORE_SUBJECT_DEFAULT = [
-    '_', // BHYT, Đồng phục thể dục
-    'MEETING', // Sinh hoạt lớp
-    'PEDU', // Giáo dục thể chất
-    'DEDU', // Giáo dục quốc phòng
-    'TEST', // Kiểm tra đầu vào
-    'GENG0', // Tiếng Anh căn bản
-    'GENG4' // Tiếng Anh đầu ra
+  "_", // BHYT, Đồng phục thể dục
+  "MEETING", // Sinh hoạt lớp
+  "PEDU", // Giáo dục thể chất
+  "DEDU", // Giáo dục quốc phòng
+  "TEST", // Kiểm tra đầu vào
+  "GENG0", // Tiếng Anh căn bản
+  "GENG4" // Tiếng Anh đầu ra
 ];
 
-export const _CAPYBARA = 'capybara';
+export const _CAPYBARA = "capybara";
 
 export const _CAPYBARA_DEFAULT = {
-    ignoreList: _IGNORE_SUBJECT_DEFAULT,
-    updatedAt: new Date(),
-    isOnlyCalcGPA: false,
-    queyText: '',
-    data: []
+  fullName: "",
+  userId: "",
+  ignoreList: _IGNORE_SUBJECT_DEFAULT,
+  updatedAt: new Date(),
+  isOnlyCalcGPA: false,
+  queyText: "",
+  data: []
 };
 
-export const _URL_TIENICHSV = 'https://tienichsv.ou.edu.vn/#/diem';
+export const _URL_TIENICHSV = "https://tienichsv.ou.edu.vn";
+
+export const _DEBOUNCE_TIME = 1000;
+export const _ERROR_MESSAGE_TIMEOUT = 5000;
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,6 +1,6 @@
 // https://developer.chrome.com/docs/extensions/mv3/messaging/
 
-import { _CAPYBARA, _CAPYBARA_DEFAULT } from './constant.js';
+import { _CAPYBARA, _CAPYBARA_DEFAULT, _DEBOUNCE_TIME } from "./constant.js";
 
 /**
  *
@@ -11,57 +11,58 @@ import { _CAPYBARA, _CAPYBARA_DEFAULT } from './constant.js';
  */
 
 export const injectScriptActiveTab = (injectScript) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const tab = tabs[0];
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
 
-        chrome.scripting
-            .executeScript({
-                target: { tabId: tab.id },
-                func: injectScript
-            })
-            .then(() => console.log('Injected!'));
-    });
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id },
+        func: injectScript
+      })
+      .then(() => console.info("Injected JS!"))
+      .catch((err) => console.error(err));
+  });
 };
 
-export function debounce(func, timeout = 1000) {
-    let timer;
-    return (...args) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            func.apply(this, args);
-        }, timeout);
-    };
+export function debounce(func, timeout = _DEBOUNCE_TIME) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
 
 export function removeVietnameseTones(str) {
-    return str
-        .normalize('NFD') // Chuẩn hóa chuỗi theo dạng chuẩn Unicode
-        .replace(/[\u0300-\u036f]/g, '') // Loại bỏ các dấu tổ hợp
-        .replace(/đ/g, 'd') // Thay thế chữ "đ"
-        .replace(/Đ/g, 'D') // Thay thế chữ "Đ"
-        .replace(/[^\w\s]/gi, '') // Loại bỏ các ký tự đặc biệt
-        .toLowerCase(); // Chuyển tất cả thành chữ thường
+  return str
+    .normalize("NFD") // Chuẩn hóa chuỗi theo dạng chuẩn Unicode
+    .replace(/[\u0300-\u036f]/g, "") // Loại bỏ các dấu tổ hợp
+    .replace(/đ/g, "d") // Thay thế chữ "đ"
+    .replace(/Đ/g, "D") // Thay thế chữ "Đ"
+    .replace(/[^\w\s]/gi, "") // Loại bỏ các ký tự đặc biệt
+    .toLowerCase(); // Chuyển tất cả thành chữ thường
 }
 
 // capybara
 export function getCapybara() {
-    return JSON.parse(localStorage.getItem(_CAPYBARA)) || _CAPYBARA_DEFAULT;
+  return JSON.parse(localStorage.getItem(_CAPYBARA)) || _CAPYBARA_DEFAULT;
 }
 
 export function setCapybara(data) {
-    localStorage.setItem(_CAPYBARA, JSON.stringify(data));
+  localStorage.setItem(_CAPYBARA, JSON.stringify(data));
 }
 
 export function getCapybaraKey(key) {
-    return getCapybara()[key];
+  return getCapybara()[key];
 }
 
 export function setCapybaraKey(key, value) {
-    const capybara = getCapybara();
-    capybara[key] = value;
-    setCapybara(capybara);
+  const capybara = getCapybara();
+  capybara[key] = value;
+  setCapybara(capybara);
 }
 
 export function clearCapybara() {
-    localStorage.removeItem(_CAPYBARA);
+  localStorage.removeItem(_CAPYBARA);
 }
