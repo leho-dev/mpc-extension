64bee8423515d5becb94e1c05df33cb1a10168ed
leho-dev <phanleho002@gmail.com>
2024-08-12 10:36:15 +0700

    chore: require conventional commit

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // A new feature
+        "fix", // A bug fix
+        "docs", // Documentation only changes
+        "chore", // Changes to the build process or auxiliary tools and libraries such as documentation generation
+        "style", // Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+        "refactor", // A code change that neither fixes a bug nor adds a feature
+        "ci", // Changes to our CI configuration files and scripts
+        "test", // Adding missing tests or correcting existing tests
+        "revert", // Reverts a previous commit
+        "perf", // A code change that improves performance
+        "emotion" // A code change depend on emotion
+      ]
+    ]
+  }
+};
---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.webextensions } },
+    rules: {
+      "no-undef": "error",
+      "no-console": [
+        "error",
+        {
+          allow: ["info", "error"]
+        }
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_"
+        }
+      ]
+    },
+    ignores: ["node_modules", "commitlint.config.js"]
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier
+];
