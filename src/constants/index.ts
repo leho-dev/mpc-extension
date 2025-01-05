ec92d9eb11632c0b1f4cc9ff397f6732f65590f3
hole <phanleho002@gmail.com>
2025-01-05 23:56:50 +0700

    fix(fake): add footer info

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,6 @@ export const _ERROR_MESSAGE_TIMEOUT: number = 5000;
 
 export const _ACTION_CREATE = "create" as const;
 export const _ACTION_UPDATE = "update" as const;
+
+export const _FB_HOMEPAGE_URL: string = "https://www.facebook.com/CLBLapTrinhTrenThietBiDiDong";
+export const _GITHUB_URL: string = "https://github.com/leho-dev/mpc-extension";
